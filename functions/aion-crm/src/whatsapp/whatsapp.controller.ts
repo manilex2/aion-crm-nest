2d67fe168766132d070c74242540b9e13f27d383
Manuel Pereira <manilex2@gmail.com>
2025-06-30 04:12:10 -0400

    Cambios en funciones de WhatsApp

--- functions/aion-crm/src/whatsapp/whatsapp.controller.ts
+++ functions/aion-crm/src/whatsapp/whatsapp.controller.ts
@@ -33,4 +33,46 @@ export class WhatsappController {
       });
     }
   }
+
+  @Get('send-notifications-pre')
+  @Header('Content-Type', 'application/json')
+  async sendNotificationsPre(@Res() res: Response) {
+    try {
+      await this.whatsappService.sendNotificationsPrev();
+      return res
+        .status(HttpStatus.CREATED)
+        .send({ message: 'Notificaciones enviadas exitosamente' });
+    } catch (error) {
+      // Si el error es de tipo HttpException, usamos su código de estado
+      if (error instanceof HttpException) {
+        return res.status(error.getStatus()).send({ message: error.message });
+      }
+      // Si no es un HttpException, retornamos un error genérico 500
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: 'Error al enviar las notificaciones',
+        error: error.message,
+      });
+    }
+  }
+
+  @Get('send-notifications-pos')
+  @Header('Content-Type', 'application/json')
+  async sendNotificationsPos(@Res() res: Response) {
+    try {
+      await this.whatsappService.sendNotificationsPos();
+      return res
+        .status(HttpStatus.CREATED)
+        .send({ message: 'Notificaciones enviadas exitosamente' });
+    } catch (error) {
+      // Si el error es de tipo HttpException, usamos su código de estado
+      if (error instanceof HttpException) {
+        return res.status(error.getStatus()).send({ message: error.message });
+      }
+      // Si no es un HttpException, retornamos un error genérico 500
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: 'Error al enviar las notificaciones',
+        error: error.message,
+      });
+    }
+  }
 }
